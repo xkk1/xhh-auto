d63c3ad6811fd2d73ca3bd7b7cd51dd688386f79
xkk1 <xkk1@120107.xyz>
2025-09-26 17:09:09 +0800

    refactor: 将 任务管理器.py 从 小红狐/核心 移动的 小红狐/工具

--- 小红狐/__main__.py
+++ 小红狐/__main__.py
@@ -8,12 +8,8 @@ def 主函数():
     # 当前目录
     小红狐脚本目录 = pathlib.Path(__file__).parent.resolve()
     启动器目录 = 小红狐脚本目录.parent
-    # 设置一个临时环境变量指定浏览器存放目录
-    os.environ["PLAYWRIGHT_BROWSERS_PATH"] = os.environ.get("PLAYWRIGHT_BROWSERS_PATH", (启动器目录 / "浏览器").as_posix())
     # 设置数据目录
     os.environ["DATA_DIR"] = os.environ.get("DATA_DIR", (启动器目录 / "数据").as_posix())
-    # 设置脚本目录
-    os.environ["SCRIPT_DIR"] = os.environ.get("SCRIPT_DIR", 启动器目录.as_posix())
     # 运行脚本
     元梦之星小红狐主函数()
 
