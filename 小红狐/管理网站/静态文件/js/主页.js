d185d25db666c75f2d0f89e830232680034c634e
xkk1 <xkk1@120107.xyz>
2025-10-15 17:50:17 +0800

    feat(管理网站): 阻止导航栏 a 标签默认事件

--- 小红狐/管理网站/静态文件/js/主页.js
+++ 小红狐/管理网站/静态文件/js/主页.js
@@ -19,4 +19,20 @@ document.addEventListener('DOMContentLoaded', function () {
             toggleNav();
         }
     });
+    
+    // 事件委托阻止 #nav 下链接 a.open-page 点击事件冒泡
+    nav.addEventListener('click', function (event) {
+        if (event.target.tagName === 'A') {
+            // 进一步判断该 <a> 是否包含 class="open-page"
+            if (event.target.classList.contains('open-page')) {
+                event.preventDefault(); // 阻止默认跳转行为
+
+                var href = event.target.getAttribute('href'); // 获取链接地址
+                console.log('阻止跳转，open-page 链接是：', href);
+
+                // 你也可以弹窗提示或做其他逻辑
+                // alert('特殊链接，地址为：' + href);
+            }
+        }
+    });
 });
