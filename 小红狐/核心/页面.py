aac86fd62ae8ed49be2d43e2df6021be818e7c42
xkk1 <xkk1@120107.xyz>
2025-10-17 16:41:32 +0800

    feat(页面管理): 增强页面目录结构支持脚本模块子目录
    新增 `脚本模块名` 参数到 `获取页面目录` 函数，以支持按模块细分页面数据存储路径。
    同时调整相关函数调用方式，确保参数传递明确一致。新增 `获取页面数据` 函数，
    用于读取指定页面及模块下的 JSON 数据文件。修复部分函数中页面名传参不完整的问题。

--- 小红狐/核心/页面.py
+++ 小红狐/核心/页面.py
@@ -2,17 +2,25 @@ import pathlib
 import shutil
 from typing import Any
 
-from 小红狐.工具.数据工具 import 数据类, 获取本地数据
+from .. import __package__ as 模块名
+from ..工具.数据工具 import 数据类, 获取本地数据
 from ..工具.目录工具 import 页面目录
 
 
 页面目录.mkdir(parents=True, exist_ok=True)
 
-def 获取页面目录(页面名: str | None = None) -> pathlib.Path:
-    if 页面名:
+def 获取页面目录(页面名: str | None = None, 脚本模块名: str | None = None) -> pathlib.Path:
+    if 页面名 and 脚本模块名:
+        return 页面目录 / 页面名 / 脚本模块名
+    elif 页面名 and not 脚本模块名:
         return 页面目录 / 页面名
+    elif not 页面名 and 脚本模块名:
+        raise ValueError("页面名不能为空")
     else:
         return 页面目录
+
+def 获取页面数据(页面名: str | None = None, 脚本模块名: str | None = None, 默认值: dict[str, Any] | None = None) -> 数据类:
+    return 获取本地数据(获取页面目录(页面名=页面名, 脚本模块名=脚本模块名) / "数据.json", 默认值=默认值)
 
 def 获取全部页面名():
     return [页面名.name for 页面名 in 获取页面目录().iterdir()]
@@ -20,27 +28,24 @@ def 获取全部页面名():
 def 新建页面(页面名: str) -> pathlib.Path:
     if 页面名 in 获取全部页面名():
         raise FileExistsError(f"页面 {页面名} 已经存在")
-    新建页面目录 = 获取页面目录(页面名)
+    新建页面目录 = 获取页面目录(页面名=页面名)
     新建页面目录.mkdir()
 
 def 删除页面(页面名: str) -> None:
     if 页面名 not in 获取全部页面名():
         raise FileNotFoundError(f"页面 {页面名} 不存在")
-    shutil.rmtree(获取页面目录(页面名))
+    shutil.rmtree(获取页面目录(页面名=页面名))
 
 def 修改页面名(页面名: str, 新页面名: str) -> None:
     if 新页面名 in 获取全部页面名():
         raise FileExistsError(f"页面 {新页面名} 已经存在")
     if 页面名 not in 获取全部页面名():
         raise FileNotFoundError(f"页面 {页面名} 不存在")
-    shutil.move(获取页面目录(页面名), 获取页面目录(新页面名))
+    shutil.move(获取页面目录(页面名=页面名), 获取页面目录(页面名=新页面名))
 
 def 复制页面(页面名: str, 新页面名: str) -> None:
     if 新页面名 in 获取全部页面名():
         raise FileExistsError(f"页面 {新页面名} 已经存在")
     if 页面名 not in 获取全部页面名():
         raise FileNotFoundError(f"页面 {页面名} 不存在")
-    shutil.copytree(获取页面目录(页面名), 获取页面目录(新页面名))
-
-def 获取页面数据(页面名: str | None = None, 默认值: dict[str, Any] | None = None) -> 数据类:
-    return 获取本地数据(获取页面目录(页面名) / "数据.json", 默认值=默认值)
+    shutil.copytree(获取页面目录(页面名=页面名), 获取页面目录(页面名=新页面名))
