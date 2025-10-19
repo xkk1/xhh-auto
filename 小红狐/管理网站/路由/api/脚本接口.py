29a707c93eecbca79994607b531cc93ea8edbced
xkk1 <xkk1@120107.xyz>
2025-10-19 16:43:10 +0800

    refactor(api): 重构脚本相关接口为脚本接口
    将原有的脚本蓝图重命名为脚本接口蓝图，并更新了对应的 URL 前缀和引用，
    以更准确地反映其功能。同时，文件名也进行了相应调整，从“脚本.py”改为
    “脚本接口.py”，确保模块名称与接口蓝图一致。

--- 小红狐/管理网站/路由/api/脚本接口.py
+++ 小红狐/管理网站/路由/api/脚本接口.py
@@ -5,10 +5,10 @@ from ....核心.脚本 import 获取导入脚本模块名列表, 获取脚本
 
 
 日志 = 获取日志记录器(__name__)
-脚本蓝图 = Blueprint(
-    "脚本",
+脚本接口蓝图 = Blueprint(
+    "脚本接口",
     __name__,
-    url_prefix="/脚本"
+    url_prefix="/脚本接口"
 )
 HTTP方法 = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD']
 
@@ -20,13 +20,13 @@ def 处理接口(脚本模块名, 子路径):
         return jsonify({"错误": "脚本未启用 Flask"}), 400
     return 脚本["flask_route"](子路径=子路径)
 
-@脚本蓝图.route("/<package>/<path:subpath>", methods=HTTP方法)
+@脚本接口蓝图.route("/<package>/<path:subpath>", methods=HTTP方法)
 def 处理子路径(package, subpath):
     return 处理接口(脚本模块名=package, 子路径="/"+subpath)
-@脚本蓝图.route("/<package>/", methods=HTTP方法)
+@脚本接口蓝图.route("/<package>/", methods=HTTP方法)
 def 处理路径(package):
     return 处理接口(脚本模块名=package, 子路径="/")
 
-@脚本蓝图.route("/<package>", methods=HTTP方法)
+@脚本接口蓝图.route("/<package>", methods=HTTP方法)
 def 处理模块(package):
     return 处理接口(脚本模块名=package, 子路径="")
