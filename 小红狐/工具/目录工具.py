9a44ad3c55a1cb13dcb61e153137a41973ec0985
xkk1 <xkk1@120107.xyz>
2025-10-24 15:17:04 +0800

    feat(工具): 调整账号和页面目录路径 将账号目录和页面目录的路径调整至全局配置目录下的小红狐模块名子目录中， 以更好地组织配置文件结构。

--- 小红狐/工具/目录工具.py
+++ 小红狐/工具/目录工具.py
@@ -1,13 +1,15 @@
 import os
 from pathlib import Path
 
+from .. import __package__ as 小红狐模块名
+
 
 数据目录 = Path(os.environ.get("DATA_PATH", "数据")).resolve()
 日志目录 = 数据目录 / "日志"
-账号目录 = 数据目录 / "账号"
-页面目录 = 数据目录 / "页面"
 配置目录 = 数据目录 / "配置"
 全局配置目录 = 数据目录 / "全局配置"
+账号目录 = 全局配置目录 / 小红狐模块名 / "账号"
+页面目录 = 全局配置目录 / 小红狐模块名 / "页面"
 
 小红狐目录 = Path(__file__).parent.parent.resolve()
 脚本目录 = 小红狐目录 / "脚本"
