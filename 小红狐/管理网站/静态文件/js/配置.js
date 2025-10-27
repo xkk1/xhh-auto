bbc4883360ddae42df410f1f5dc12a7e0bf4f486
xkk1 <xkk1@120107.xyz>
2025-10-27 15:58:48 +0800

    feat(页面配置): 新增多页面功能
    在页面配置标签页功能中，为每个标签页链接添加了 "打开页面" 样式类，
    以便于样式控制与页面状态标识。
    
    同时新增初始化多页面功能，用于加载并管理多个页面的切换逻辑。
    在 DOM 加载完成后调用该初始化函数，以确保页面结构正确渲染。

--- 小红狐/管理网站/静态文件/js/配置.js
+++ 小红狐/管理网站/静态文件/js/配置.js
@@ -13,6 +13,7 @@ function 页面配置标签页() {
                 let a = document.createElement("a");
                 a.href = 标签页.url;
                 a.textContent = 标签页.标题;
+                a.classList.add("打开页面");
                 a.role = "tab";
                 let li = document.createElement("li");
                 li.appendChild(a);
@@ -27,10 +28,19 @@ function 页面配置标签页() {
         });
 }
 
+// 初始化多页面
+function 初始化多页面() {
+    let iframe容器元素 = document.querySelector("#main");
+    多页面 = 小红狐工具.获取多页面(iframe容器元素);
+    window.多页面 = 多页面;
+    let 标签页父元素 = document.querySelector("ul#标签页");
+    小红狐工具.多页面切换(标签页父元素, 多页面.打开页面);
+}
+
 // DOM 加载完成时执行
 document.addEventListener("DOMContentLoaded", function () {
     // 显示页面名
     document.querySelector("#页面名").textContent = 页面名;
+    初始化多页面();
     页面配置标签页();
-
 });
