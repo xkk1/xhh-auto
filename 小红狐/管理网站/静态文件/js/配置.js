dc888b5c1c805e5282faa72c65cc9a8c8c9a35ed
xkk1 <xkk1@120107.xyz>
2025-10-27 16:08:19 +0800

    feat(多页面管理): 实现多页面初始化与标签页切换功能
    更改初始化多页面函数定义位置，用于设置 iframe 容器和标签页切换逻辑。
    在页面配置标签页完成后，自动打开第一个标签页并显示标签页区域，
    同时隐藏加载提示元素，提升用户界面交互体验。

--- 小红狐/管理网站/静态文件/js/配置.js
+++ 小红狐/管理网站/静态文件/js/配置.js
@@ -3,6 +3,15 @@ let 页面名 = 小红狐工具.获取Get参数("页面名") || "页面名六个
 // 标题添加页面名
 document.title = 页面名 + "-" + document.title;
 
+// 初始化多页面
+function 初始化多页面() {
+    let iframe容器元素 = document.querySelector("#main");
+    多页面 = 小红狐工具.获取多页面(iframe容器元素);
+    window.多页面 = 多页面;
+    let 标签页父元素 = document.querySelector("ul#标签页");
+    小红狐工具.多页面切换(标签页父元素, 多页面.打开页面);
+}
+
 // 获取并设置页面配置标签页
 function 页面配置标签页() {
     let 标签页元素 = document.querySelector("ul#标签页");
@@ -20,21 +29,16 @@ function 页面配置标签页() {
                 li.role = "none";
                 标签页元素.appendChild(li);
             }
+            // 打开第一个标签页
+            document.querySelector("ul#标签页>li>a").click();
+            // 显示标签页
             document.querySelector("#header").style.display = "block";
+            // 隐藏加载
             document.querySelector("#加载").style.display = "none";
         })
         .catch(function (错误) {
             console.error(错误);
         });
-}
-
-// 初始化多页面
-function 初始化多页面() {
-    let iframe容器元素 = document.querySelector("#main");
-    多页面 = 小红狐工具.获取多页面(iframe容器元素);
-    window.多页面 = 多页面;
-    let 标签页父元素 = document.querySelector("ul#标签页");
-    小红狐工具.多页面切换(标签页父元素, 多页面.打开页面);
 }
 
 // DOM 加载完成时执行
