f95037130e6cb06a4559100cc5de0431e7658413
xkk1 <xkk1@120107.xyz>
2025-10-28 20:37:41 +0800

    feat(api): 添加配置数据获取接口
    新增根据配置名和模块名获取配置数据的接口，支持通过路由参数传递配置名和模块名。
    同时支持从请求体中读取默认值，增强接口灵活性。

--- 小红狐/管理网站/路由/api/配置.py
+++ 小红狐/管理网站/路由/api/配置.py
@@ -1,7 +1,9 @@
+from typing import Any
 from flask import Blueprint, jsonify, request
 
+from .... import __package__ as 小红狐模块名
 from ....工具.日志工具 import 获取日志记录器
-from ....核心.配置 import 获取全部配置名
+from ....核心.配置 import 获取全部配置名, 获取配置数据
 
 
 日志 = 获取日志记录器(__name__)
@@ -12,7 +14,27 @@ from ....核心.配置 import 获取全部配置名
 )
 
 
-# 返回内存数据 json
 @配置蓝图.route("/配置名", methods=["GET"])
-def 内存数据():
+def 全部配置名():
     return jsonify(获取全部配置名())
+
+# 获取配置数据
+def 配置数据(配置名: str = "默认", 脚本模块名: str = 小红狐模块名):
+    默认值: dict[str, Any] = {}
+    json_data = request.get_json(silent=True)  # JSON 请求体，silent=True 避免解析失败报错
+    if json_data:
+        默认值 = json_data
+        print(默认值)
+    return jsonify(获取配置数据(配置名=配置名, 脚本模块名=脚本模块名, 默认值=默认值).数据)
+
+
+@配置蓝图.route("/数据/<config_name>", methods=["GET"])
+def 小红狐配置数据(config_name):
+    配置名: str = config_name
+    return 配置数据(配置名=配置名, 脚本模块名=小红狐模块名)
+
+@配置蓝图.route("/数据/<config_name>/<package_name>", methods=["GET"])
+def 脚本配置数据(config_name, package_name):
+    配置名: str = config_name
+    脚本模块名: str = package_name
+    return 配置数据(配置名=配置名, 脚本模块名=脚本模块名)
