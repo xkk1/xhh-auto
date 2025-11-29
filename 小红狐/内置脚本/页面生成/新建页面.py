92a19a69cc893f0b401fcf89b3a3a61ac165b56f
xkk1 <xkk1@120107.xyz>
2025-11-29 18:42:46 +0800

    feat(页面生成): 重构新建页面逻辑以支持异步操作
    将新建页面函数拆分为异步版本和同步版本，优化页面初始化流程。
    异步函数中增加对页面初始 URL 的访问处理，并传递更多上下文参数以控制页面行为。

--- 小红狐/内置脚本/页面生成/新建页面.py
+++ 小红狐/内置脚本/页面生成/新建页面.py
@@ -11,11 +11,22 @@ from ...工具.任务管理器工具 import 异步任务管理器
 名称: str = "小红狐新建页面"
 
 
-def 新建页面(页面名: str | None = None) -> playwright.async_api.Page:
+async def 异步新建页面(页面名: str) -> playwright.async_api.Page:
+    from ...核心.页面 import 获取页面账号名, 获取页面初始URL
     from ...核心.浏览器 import 新建页面 as 小红狐新建页面
-    from ...核心.页面 import 获取页面账号名
     账号名: str = 获取页面账号名(页面名)
-    return 异步任务管理器.运行(小红狐新建页面(页面名=页面名, 账号名=账号名))
+    页面: playwright.async_api.Page = await 小红狐新建页面(
+        页面名=页面名, 账号名=账号名, 
+        kwargs上下文={"no_viewport": True, "color_scheme": "no-preference"}
+    )
+    # 访问目标网页
+    页面初始URL: str = 获取页面初始URL(页面名)
+    if 页面初始URL != "":
+        await 页面.goto(页面初始URL)
+    return 页面
+
+def 新建页面(页面名: str | None = None) -> playwright.async_api.Page:
+    return 异步任务管理器.运行(异步新建页面(页面名=页面名))
 
 def 关闭页面(页面名: str | None = None):
     from ...核心.浏览器 import 关闭页面 as 小红狐关闭页面
