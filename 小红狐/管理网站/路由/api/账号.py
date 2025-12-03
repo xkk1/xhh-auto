6e7b6db462e72f1d3d14450172db9dad7b68189a
xkk1 <xkk1@120107.xyz>
2025-12-03 22:40:38 +0800

    feat(账号管理): 添加删除账号功能
    新增了通过 DELETE 请求删除指定账号的功能，完善了账号管理 API。
    同时更新了相关模块导入语句以支持新功能调用。

--- 小红狐/管理网站/路由/api/账号.py
+++ 小红狐/管理网站/路由/api/账号.py
@@ -4,7 +4,7 @@ from flask import Blueprint, jsonify, request
 
 from .... import __package__ as 小红狐模块名
 from ....工具.日志工具 import 获取日志记录器
-from ....核心.账号 import 保存账号状态, 修改账号名, 新建账号, 获取全部账号名
+from ....核心.账号 import 保存账号状态, 修改账号名, 删除账号, 新建账号, 获取全部账号名
 
 
 日志 = 获取日志记录器(__name__)
@@ -62,6 +62,17 @@ def 修改账号名路由():
     except Exception as e:
         日志.警告(f"修改账号名失败，账号名：“{账号名}”: {e}")
         return jsonify(f"修改账号名失败!\n账号名“{账号名}”: {e}"), 500
+
+# 删除账号
+@账号蓝图.route("/账号名/<account_name>", methods=["DELETE"])
+def 删除账号路由(account_name: str):
+    账号名: str = account_name
+    try:
+        删除账号(账号名=账号名)
+        return jsonify("删除账号成功")
+    except Exception as e:
+        日志.警告(f"删除账号失败，账号名：“{账号名}”: {e}")
+        return jsonify(f"删除账号失败!\n账号名“{账号名}”: {e}"), 500
 
 @账号蓝图.route("/保存账号状态/<account_name>", methods=["GET"])
 def 保存账号状态路由(account_name: str):
